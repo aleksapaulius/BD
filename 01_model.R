a8fb06c12ffd6c0964086933671ea5a2d5709ad0
aleksapaulius <apauliui@gmail.com>
2018-05-22 00:00:24 +0300

    commit 20180521

---
+++ 01_model.R
@@ -0,0 +1,20 @@
+# MODEL ------------------------------------------------------------------------
+
+# paruosiam duomenis modeliavimui
+alldata.model <- alldata.m[,!(names(alldata.m) %in% c('loan_interest_LTL', 'loan_value_LTL', 'deposit_interest_LTL'))]
+diff1.variables <- setdiff(diff1.variables, c('loan_interest_LTL', 'loan_value_LTL', 'deposit_interest_LTL', 'cash', 'deposits'))
+alldata.model[1:nrow(alldata.model)-1, diff1.variables] <- lapply(alldata.m[diff1.variables], diff)
+alldata.model[nrow(alldata.model),diff1.variables] <- NA
+alldata.model['ratio'] <- alldata.model$cash/alldata.model$deposits
+
+EUR.dates <- c(apply(expand.grid(2015:2017, 1:9),1,paste,collapse=" 0"), apply(expand.grid(2015:2017, 10:12),1,paste,collapse=" "))
+alldata.model[alldata.model$date %in% EUR.dates, 'dummy_EUR'] <- 1
+alldata.model[is.na(alldata.model$dummy_EUR), 'dummy_EUR'] <- 0
+
+alldata.model <- alldata.model[complete.cases(alldata.model),]
+
+# creating formula and model
+lm.regressors1 <- setdiff(names(alldata.model), c('date', 'cash', 'deposits', 'ratio'))
+f1 <- as.formula(paste("ratio ~ ", paste(lm.regressors1, collapse=" + ")))
+fit1 <- lm(f1, data = alldata.model)
+#summary(fit1)
