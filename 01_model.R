c58ed0b4392766d8fb5e92fca6574f2d921d2826
aleksapaulius <apauliui@gmail.com>
2018-05-30 23:19:00 +0300

    commit 20180530

--- 01_model.R
+++ 01_model.R
@@ -24,6 +24,26 @@ for (var.name in setdiff(unique(names(alldata.model)), c('santykis', 'dummy_EUR'
 names(alldata.model) <- gsub(" ", "_", names(alldata.model))
 names(alldata.model) <- gsub(":", "_", names(alldata.model))
 
+# changing scale of variables
+alldata.model$Gautų_mokėjimų_vertė <- alldata.model$Gautų_mokėjimų_vertė / 100000
+alldata.model$Išeinančių_mokėjimų_skaičius <- alldata.model$Išeinančių_mokėjimų_skaičius / 100
+alldata.model$Grynųjų_įnešimo_operacijų_vertė <- alldata.model$Grynųjų_įnešimo_operacijų_vertė / 100000
+alldata.model$Grynųjų_įnešimo_operacijų_vertė <- alldata.model$Grynųjų_įnešimo_operacijų_vertė / 100000
+alldata.model$Grynųjų_išėmimo_operacijų_skaičius <- alldata.model$Grynųjų_išėmimo_operacijų_skaičius / 100
+alldata.model$Grynųjų_išėmimo_operacijų_vertė <- alldata.model$Grynųjų_išėmimo_operacijų_vertė / 10000
+alldata.model$Grynųjų_išėmimo_operacijų_vertė <- alldata.model$Grynųjų_išėmimo_operacijų_vertė / 10000
+alldata.model$Kredito_kortelių_skaičius <- alldata.model$Kredito_kortelių_skaičius / 100
+alldata.model$Debeto_kortelių_skaičius <- alldata.model$Debeto_kortelių_skaičius / 100000
+alldata.model$GPM <- alldata.model$GPM / 1000
+alldata.model$GPM <- alldata.model$GPM / 1000
+alldata.model$Akcizo_mokesčiai <- alldata.model$Akcizo_mokesčiai / 100
+alldata.model$Pelno_mokesčiai <- alldata.model$Pelno_mokesčiai / 10000
+alldata.model$Pridėtinės_vertės_mokesčiai <- alldata.model$Pridėtinės_vertės_mokesčiai / 1000
+alldata.model$Pridėtinės_vertės_mokesčiai <- alldata.model$Pridėtinės_vertės_mokesčiai / 1000
+alldata.model$santykis <- alldata.model$santykis * 10
+
+
+
 
 # MODEL 1
 lm.regressors1 <- setdiff(names(alldata.model), 'santykis')
